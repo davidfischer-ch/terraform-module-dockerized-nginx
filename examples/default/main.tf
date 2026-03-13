bd3b8ff8b3935360c677afe2df0ebfd62608fa2d
David Fischer <david@fisch3r.net>
2026-03-13 16:14:10 +0100

    Update URLs in examples

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -30,7 +30,7 @@ resource "docker_network" "app" {
 }
 
 module "reverse_proxy" {
-  source = "git::https://github.com/davidfischer-ch/terraform-module-dockerized-nginx.git?ref=1.1.2"
+  source = "git::https://github.com/davidfischer-ch/terraform-module-dockerized-nginx.git?ref=1.2.0"
 
   identifier     = "my-app-reverse-proxy"
   image_id       = docker_image.nginx.image_id
